e0ac0cce6f30c0d7603df4acf7143b47514a2fd5
JO Gyeongmin <gorogara87@gmail.com>
2024-09-20 11:16:29 +0900

    Init

---
+++ Doxygen_test/TestClass.cpp
@@ -0,0 +1,16 @@
+#include "TestClass.h"
+
+TestClass::TestClass()
+{
+
+}
+
+TestClass::~TestClass()
+{
+
+}
+
+int TestClass::testMethod(int a, int b)
+{
+	return a + b;
+}
---
+++ Doxygen_test/TestClass.h
@@ -0,0 +1,16 @@
+#pragma once
+class TestClass
+{
+public:
+	TestClass();
+	~TestClass();
+
+	/**
+	* @brief This is a test method, ¥ı«œ±‚
+	* @param a The first parameter
+	* @param b The second parameter
+	* @return The sum of the two parameters
+	*/
+	int testMethod(int a, int b);
+};
+
